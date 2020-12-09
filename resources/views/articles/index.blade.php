fead1f97dd311d3717e9016fdcb81a8766b365d9
Alexandr Khokhanov <hohanov@gmail.com>
2020-12-09 11:08:26 +0300

    lesson 9: fix article show view

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -8,7 +8,7 @@
 
     <article>
         <h2>
-            <a href="/articles/{{ $article->id }}">{{ $article->title }}</a>
+            <a href="{{ action ('ArticlesController@show', [$article->id]) }}">{{ $article->title }}</a>
         </h2>
         <div class="body">{{ $article->body }}</div>
     </article>
