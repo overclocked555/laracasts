6ad881650bf9e76db596949e1ed2623baba1110b
Alexandr Khokhanov <hohanov@gmail.com>
2020-12-15 21:03:51 +0300

    lesson 10 finished (forms)

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,11 +14,13 @@
 Route::get('/', 'WelcomeController@index');
 
 Route::get('contact', 'PagesController@contact');
-
 Route::get('about', 'PagesController@about');
 
 Route::get('articles', 'ArticlesController@index');
+Route::get('articles/create', 'ArticlesController@create');
 Route::get('articles/{id}', 'ArticlesController@show');
+Route::post('articles', 'ArticlesController@store');
+
 
 Route::get('home', 'HomeController@index');
 
